0467a19aff7cdf129531d06709c3e8f58960d099
Khushboo <patelkhushboo057@gmail.com>
2024-08-02 01:11:39 +0530

    implemented with middleware logger (checkk console)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 
 import './App.css';
-import ToDoList from './ToDoList';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, removeTodo } from './actions';
+
 
 function App() {
+  const [input, setInput] = useState('');
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos.todos);
+
+  const handleAdd = () => {
+    if (input.trim()) {
+      dispatch(addTodo(input.trim()));
+      setInput('');
+    }
+  };
+
+  const handleRemove = (index) => {
+    dispatch(removeTodo(index));
+  };
+
   return (
-    <div className="App">
-     <ToDoList/>
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleAdd}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => handleRemove(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
